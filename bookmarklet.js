228117653f007498c0f84aa1cf13b0c5bd644bbb
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2015-03-12 14:22:34 -0500

    Remove unnecessary code.

--- bookmarklet.js
+++ bookmarklet.js
@@ -5,7 +5,5 @@ function f() {
 }
 // This is the code that sets up the bookmarklet.
 document.getElementById("bookmarklet").href = "javascript:(" + f.toString() + ")();"
-// Let any other scripts know that the script was loaded successfully.
-var document.bookmarkletLoaded = true;
 // https://juhukinners.wordpress.com/2009/01/08/how-to-write-a-bookmarklet/
 // TODO: check length limits? 
